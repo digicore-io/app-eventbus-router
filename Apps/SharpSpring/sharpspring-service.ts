5175663e91b344bfa0a701bebcad70ab1688d54d
James Scott <james@digipromedia.com>
2020-02-18 21:30:44 +1100

    AEB-76 - Added slack logging to new SS channel

--- Apps/SharpSpring/sharpspring-service.ts
+++ Apps/SharpSpring/sharpspring-service.ts
@@ -1,10 +1,12 @@
 import { EvendtDao } from "./dao/event-dao";
+import { BaseService } from "@digicore-io/digicore-node-common-module/lib/base-service";
+import { Constant } from "./constants";
 const uuidv4 = require("uuid/v4");
 const AWS = require("aws-sdk");
 const axios = require("axios");
 const eventDao = new EvendtDao();
 
-export class SharpSpringService {
+export class SharpSpringService extends BaseService {
   async processMessage(item: any) {
     let eventRequest: EventRequest = <EventRequest>JSON.parse(item.body);
     let companyAppEvent = <CompanyAppEvent>await eventDao.getCompanyAppEvent(eventRequest.companyAppEventId);
@@ -12,14 +14,22 @@ export class SharpSpringService {
     let url: string = "https://api.sharpspring.com/pubapi/v1.2?accountID=" + config.accountId + "&secretKey=" + config.secretKey;
 
     let result: any = await this.createContact(eventRequest, url);
+    let msg: string;
 
     if (result.error.length > 0 && result.error[0].code == 301) {
       if (eventRequest.payload.allowCustomerUpdate) {
         console.log("Updating contact");
         result = await this.updateContact(eventRequest, url);
-      } else console.log("Customer already exists but updates were not requested");
-    }
+        msg = new SlackMessage().createMessage(eventRequest, "UPDATE", null);
+      } else {
+        let additionalMsg = "Customer already exists but updates were not requested";
+        console.log(additionalMsg);
+        msg = new SlackMessage().createMessage(eventRequest, "UPDATE", additionalMsg);
+      }
+    } else msg = new SlackMessage().createMessage(eventRequest, "CREATE", null);
 
+    let stage = process.env.STAGE == null ? "local" : process.env.STAGE;
+    await this.logToSlack(Constant.SLACK_CHANNEL, stage, "SharpSpring App", msg);
     console.log("RESULT", result);
   }
 
@@ -45,6 +55,19 @@ export class SharpSpringService {
 
       resolve(result.data);
     });
+  }
+}
+
+export class SlackMessage {
+  createMessage(eventRequest: any, method: string, additionalMsg: string) {
+    let contact = eventRequest.payload.customer;
+    let msg: string = "Company ID: " + eventRequest.companyId + "\n" + "Email: " + contact.emailAddress;
+
+    if (additionalMsg) msg += "More detail: " + additionalMsg;
+
+    msg += "\n\nAll Details: " + JSON.stringify(contact);
+
+    return msg;
   }
 }
 
